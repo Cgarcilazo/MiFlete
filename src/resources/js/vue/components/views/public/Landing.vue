51820f39415117696d3605e8b563c03d9ec4c20f
Fernanda <fernanda@42mate.com>
2020-08-24 22:27:42 -0300

    vue-router implementation

---
+++ src/resources/js/vue/components/views/public/Landing.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="d-flex mt-5 justify-content-center">
+    <h1>
+      LANDING
+    </h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+h1 {
+  color: $green
+}
+</style>
