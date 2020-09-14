7c12cf783f2b59e98d1e8da0194b8945f98542e4
Fernanda <fernanda@42mate.com>
2020-09-13 22:12:23 -0300

    created carrier home view

--- src/resources/js/vue/components/views/carriers/Dashboard.vue
+++ src/resources/js/vue/components/views/carriers/Dashboard.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
+  <div class="h-100">
+    <Navbar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+  import Navbar from 'Components/Navbar'
   export default {
+    components: { Navbar }
 
   }
 </script>
