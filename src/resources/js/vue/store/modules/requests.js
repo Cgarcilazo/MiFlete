3ed6dff3785fbcb6be828d5c0d75bd364365453d
Cristian <cristian@42mate.com>
2020-10-11 17:37:35 -0300

    Show requests table integrated with endpoint - new loader component

---
+++ src/resources/js/vue/store/modules/requests.js
@@ -0,0 +1,45 @@
+'use strict';
+import axios from 'axios';
+
+const getDefaultState = () => {
+  return {
+    requests: [],
+  }
+};
+
+export const state = getDefaultState();
+
+export const getters = {
+
+};
+
+export const mutations = {
+  setRequests (state, requests) {
+    state.requests = requests;
+  }
+};
+
+export const actions = {
+  fetchAll ({ commit, rootGetters }) {
+    return new Promise((resolve, reject) => {
+      const userId = rootGetters['users/getUser'].id || null;
+      axios.get(`/api/v1/users/${userId}/requests`)
+        .then((response) => {
+          const requests = response.data.data.requests || [];
+          commit('setRequests', requests);
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        })
+    });
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+}
